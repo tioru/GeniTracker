60d7397bc872a4392bb31363df43dbc936c677ac
Thomas Bobin <tbobin@geotec.fr>
2025-08-12 11:33:50 +0200

    [FIX] Updated angular.json to add external img dependancies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TopbarComponent } from '../layout/topbar/topbar.component';
 import { NotificationCenterComponent } from './components/notification-center/notification-center.component';
-import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -13,16 +12,4 @@ import { environment } from '../environments/environment';
 })
 export class AppComponent {
   title = 'GeniTracker';
-  baseUrl = environment.baseUrl;
-
-  get backgroundImageUrl() {
-    return `url("${environment.baseUrl}/assets/img/Background.png")`;
-  }
-
-  ngOnInit() {
-    document.documentElement.style.setProperty(
-      '--bg-image', 
-      `url("${environment.baseUrl}/assets/img/Background.png")`
-    );
-  }
 }
