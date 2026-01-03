28df4379127fd5427cf76a2632e6bffe436eac98
Bobin Thomas <thomas.bobin@viacesi.fr>
2026-01-03 10:53:40 +0100

    [FIX] Updated environnement.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -12,5 +12,10 @@ export const environment = {
     messagingSenderId: "957189961187",
     appId: "1:957189961187:web:8972d7afda8e23887e9cd6",
     measurementId: "G-DRC67HZ5MQ"
+  },
+  writeappConfig: {
+    apiKey: "standard_74e1829a3f179aa57e94cab88354c4586e8663d74dcac74360736d000a83988129cd637ca918f4bacf3e6e309740838364b3760591596a86e7e276e6e79bed633230416f6b4331da09c2aa9400d89976d34a09746195ec5e965b7a431a011f6d31180cd3e5526c7cae58663f9b207e53db1867b127b70d2af11029d26f87c1c2",
+    projectId: "genitracker",
+    bucketId: "main-data-bucket"
   }
 }
