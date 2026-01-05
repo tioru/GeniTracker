b1c62c5ed4049899b6f28dc17c6466bd3f231c6b
Bobin Thomas <thomas.bobin@viacesi.fr>
2026-01-05 21:23:34 +0100

    [FEAT] Updated tooltip component to be on top of DOM tree when visible

--- src/app/components/tooltip/tooltip.component.ts
+++ src/app/components/tooltip/tooltip.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, ElementRef, EmbeddedViewRef, Renderer2, TemplateRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'tooltip-component',
@@ -8,15 +8,63 @@ import { Component, HostListener } from '@angular/core';
   styleUrl: './tooltip.component.scss'
 })
 export class TooltipComponent {
-  isVisible = false;
+  @ViewChild('trigger', { static: true }) trigger!: ElementRef;
+  @ViewChild('tooltipTemplate', { static: true }) tooltipTemplate!: TemplateRef<any>;
 
-  @HostListener('mouseenter')
-  onMouseEnter() {
-    this.isVisible = true;
+  public tooltipStyle = {
+    position: 'fixed',
+    top: "",
+    left: "",
+    transform: 'translate(-50%, -100%)',
+    zIndex: 10000
+  };
+
+  public isVisible = true;
+  private tooltipView? : EmbeddedViewRef<any>
+
+  constructor(
+    private renderer: Renderer2
+  ) {}
+
+  public showTooltip() : void {
+    const rect = this.trigger.nativeElement.getBoundingClientRect();
+
+    this.tooltipStyle.top = rect.top - 8 + "px"
+    this.tooltipStyle.left = rect.left + rect.width / 2 + "px"
+
+    this.isVisible = false;
+    this.tooltipView = this.tooltipTemplate.createEmbeddedView({});
+    this.tooltipView.detectChanges();
+    
+    const tooltipElement = this.tooltipView.rootNodes[0];
+    this.renderer.appendChild(document.body, tooltipElement);
+    
+    setTimeout(() => {
+      this.isVisible = true;
+      this.tooltipView?.detectChanges();
+    }, 10);
   }
 
-  @HostListener('mouseleave')
-  onMouseLeave() {
+  public hideTooltip() : void {
     this.isVisible = false;
+    this.tooltipView?.detectChanges();
+    
+    setTimeout(() => {
+      if (this.tooltipView) {
+        const tooltipElement = this.tooltipView.rootNodes[0];
+        this.renderer.removeChild(document.body, tooltipElement);
+        this.tooltipView.destroy();
+        this.tooltipView = undefined;
+      }
+    }, 200);
+  }
+
+  ngOnDestroy() {
+    if (this.tooltipView) {
+      const tooltipElement = this.tooltipView.rootNodes[0];
+      this.renderer.removeChild(document.body, tooltipElement);
+      this.tooltipView.destroy();
+      this.tooltipView = undefined;
+    }
   }
 }
